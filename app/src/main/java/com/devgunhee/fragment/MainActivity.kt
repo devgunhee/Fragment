bfdd11b8e30275817a754eb56ea6058ed61c30ab
devgunhee <devgunhee@gmail.com>
2023-07-17 00:42:45 +0900

    step_1 initial commit

--- app/src/main/java/com/devgunhee/fragment/MainActivity.kt
+++ app/src/main/java/com/devgunhee/fragment/MainActivity.kt
@@ -2,10 +2,37 @@ package com.devgunhee.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.devgunhee.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    Log.d("", "homeSelcted")
+                    return@setOnItemSelectedListener true
+                }
+                R.id.dual -> {
+                    Log.d("", "dualSelcted")
+                    return@setOnItemSelectedListener true
+                }
+                R.id.flow -> {
+                    Log.d("", "flowSelcted")
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    Log.d("", "nothing selected")
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
     }
 }
